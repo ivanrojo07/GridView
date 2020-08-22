a264e9a240e5ccfbae8ba3a2411c5b59dca52510
Ivan Rojo <ivanrojo07@gmail.com>
2020-08-22 18:00:39 -0500

    115. GridView con AdaptadorPersonalizado

--- app/src/main/java/com/example/gridview/MainActivity.kt
+++ app/src/main/java/com/example/gridview/MainActivity.kt
@@ -12,17 +12,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var frutas = ArrayList<String>()
-        frutas.add("Durazno")
-        frutas.add("Manzana")
-        frutas.add("Platano")
-        frutas.add("Sandia")
+        var frutas = ArrayList<Fruta>()
+        frutas.add(Fruta("Durazno",R.drawable.durazno))
+        frutas.add(Fruta("Manzana",R.drawable.manzana))
+        frutas.add(Fruta("Platano",R.drawable.platano))
+        frutas.add(Fruta("Sandia",R.drawable.sandia))
 
         var grid:GridView = findViewById(R.id.grid)
-        val adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,frutas)
+//        val adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,frutas)
+        val adapter = AdaptadorCustom(this,frutas)
         grid.adapter = adapter
         grid.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, i, l ->
-            Toast.makeText(this, frutas.get(i), Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, frutas.get(i).nombre, Toast.LENGTH_SHORT).show()
         }
 
     }
